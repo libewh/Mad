1f050a69762bf9385fe8bc81df394a634112d192
LW <whiteman.70@osu.edu>
2024-09-10 11:59:00 -0400

    update

--- stats.R
+++ stats.R
@@ -1,4 +1,4 @@
-#### impervious surface ~ a. bombi ####
+### 2019 api~species ###
 #----2019----#
 species <- read_csv("/Users/whiteman/Library/CloudStorage/OneDrive-TheOhioStateUniversity/School/Papers/Invert_Path/Data/2019_SpeciesStatus.csv")
 
@@ -16,6 +16,8 @@ chisq.test(table(species$bombus, species$status), correct = FALSE)
 chisq.post.hoc(x)
 
 
+#### impervious surface ~ a. bombi ####
+#----2019----#
 
 
 
